6fffb026ff8e35d39e9acaa86c620cfde679d56e
Benjamin <benjaminxchhakchhuak@gmail.com>
2023-10-23 08:35:37 +0530

    latest3

--- backend/server.js
+++ backend/server.js
@@ -29,9 +29,12 @@ app.get("/",(req,res)=>{
 
 app.post('/create',(req,res)=>{
     const sql = "INSERT INTO student (Name, Email) VALUES (?)";
+    let data = req.body;
     const values = [
-        req.body.name,
-        req.body.email
+        // req.body.name,
+        // req.body.email
+        data.name,
+        data.email
     ]
     db.query(sql, [values],(err, data)=>{
         if(err) return res.json(err);
